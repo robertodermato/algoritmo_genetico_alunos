c9407d6cf45498d3cdff3c1f0b5d53af2c9bd871
Roberto Luís Rezende <robertodermato@gmail.com>
2021-03-31 15:22:49 -0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,79 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App {
+    private final Scanner in = new Scanner(System.in);
+    private GeneticAlgorithm algoritmo1;
+
+    // atributos private aqui
+
+    public App() {
+        //inicializa atributos
+        algoritmo1 = new GeneticAlgorithm();
+        readFile();
+        fillDataInApp();
+    }
+
+    public void run() {
+        int opcao = 1;
+
+        System.out.println("\nBem vindo ao App de confraternização de turmas");
+        System.out.println("===============================");
+
+        while(opcao!= 0){       // Repetição do menu
+
+            System.out.println();
+            System.out.println("Escolha uma das opções: \n");
+
+            System.out.println("1 - Ver a execução passo a passo");
+            System.out.println("2 - Ver apenas o resultado final");
+            System.out.println("0 - sair\n");
+
+            try{       // Impede que usuário digite letra
+                opcao = in.nextInt();
+                in.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("Opção inválida, tente novamente.");
+                in.nextLine();
+                continue;
+            }
+
+
+            switch (opcao){
+                case 1 :
+                    mostraExecucaoPassoPasso();
+                    break;
+
+                case 2:
+                    mostraApenasResultadoFinal();
+                    break;
+
+                case 0:     // Sai do menu, encerra programa
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        }
+    }
+
+    private void mostraExecucaoPassoPasso(){
+        algoritmo1.runGenerations();
+    }
+
+    private void mostraApenasResultadoFinal(){
+
+    }
+
+    private void fillDataInApp(){
+        populaLista();
+    }
+
+    private void readFile(){
+
+    }
+
+    private void populaLista(){
+        System.out.println("\nForam adicionados " + " na turma A e x alunos na turma B.");
+    }
+}
